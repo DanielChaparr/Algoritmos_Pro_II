6a94522169f6015eac5a5d1bd1f5fedf4afadf5a
DanielChaparr <116587339+DanielChaparr@users.noreply.github.com>
2022-11-09 11:58:12 -0500

    Add files via upload

---
+++ EJERCICIO 5.py
@@ -0,0 +1,31 @@
+#Ejercicio 5: Tablas de verdad en Python
+
+a= input("Ingrese el primer valor: ")
+b= input("Ingrese un segundo valor: ")
+    #c= input("Ingrese el tercer valor: ")
+    #d= input("Ingrese el cuarto valor: ")
+
+#Definir las variables como enteros 
+
+a= int (a)
+b= int (b)
+    #c= int (c)
+    #d= int (d)
+
+print("Esta es la tabla de la verdad de AND") #conjunción
+print(str(a==a) + " AND " + str(b==b) + " --> " + str(a==a))
+print(str(a==a) + " AND " + str(b>b) + " --> " + str(a<a))
+print(str(a>a) + " AND " + str(b==b) + " --> " + str(b!=b))
+print(str(a==b) + " AND " + str(b<a) + " --> " + str(b!=b))
+
+print("Esta es la tabla de la verdad de OR") #disyunción
+print(str(a==a) + " OR " + str(b==b) + "--> " + str(a==a))
+print(str(a==a) + " OR " + str(b!=b) + " --> " + str(b==b))
+print(str(a<a) + " OR " + str(b==b) + " --> " + str(b==b))
+print(str(a!=a) + " OR " + str(b>b) + " --> " + str(a<a))
+
+print("Esta es la tabla de la verdad de THEN") #condicional
+print(str(a==a) + " THEN " + str(b==b) + " --> " + str(a==a))
+print(str(a==a) + " THEN " + str(b!=b) + " --> " + str(b<b))
+print(str(a<a) + " THEN " + str(b==b) + " --> " + str(b==b))
+print(str(a!=a) + " THEN " + str(b>b) + " --> " + str(a==a))
