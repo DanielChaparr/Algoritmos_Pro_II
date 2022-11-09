0f4cfd13224d735226b55198c16cc13f43725deb
DanielChaparr <116587339+DanielChaparr@users.noreply.github.com>
2022-11-09 11:57:51 -0500

    Add files via upload

---
+++ Ejercicio 8.py
@@ -0,0 +1,17 @@
+print ("Canasta familiar")
+print ("-----------------------")
+
+a = print("Huevos")
+b = print("Pollo")
+c = print("Carne")
+d = print("Harina")
+e = print ("Leche")
+f = print ("Pan")
+g = print ("Mazorca")
+
+print ("Tabla de Condicional")
+
+print (str(a==a) + "xor" + str(a==a) + "-->" + str(a==a))
+print (str(b==b) + "xor" + str(e==e) + "-->" + str(e!=b))
+print (str(c==c) + "xor" + str(f==f) + "-->" + str(f!=c))
+print (str(d==d) + "xor" + str(d==d) + "-->" + str(d==d))
