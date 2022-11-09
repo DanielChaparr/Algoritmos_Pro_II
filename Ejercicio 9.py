9598ccf0eb0e151efeecbc8d32022921b40a2e74
DanielChaparr <116587339+DanielChaparr@users.noreply.github.com>
2022-11-09 12:46:28 -0500

    Add files via upload

---
+++ Ejercicio 9.py
@@ -0,0 +1,6 @@
+while True:
+    valor_ingresado = int(input("ingrese un numero entero"))
+    if valor_ingresado % 2 !=0:
+        print("Este numero es impar")
+        break
+    print ("Este numero es par")
