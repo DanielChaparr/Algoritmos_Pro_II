9924f0deda682092d7d575acbc8c711ee16aa0fe
DanielChaparr <116587339+DanielChaparr@users.noreply.github.com>
2022-11-09 11:58:28 -0500

    Add files via upload

---
+++ #Ejercicio 6 Python.py
@@ -0,0 +1,12 @@
+#Ejercicio 6 Python
+
+print("""
+¡Buenas! ¿Qué va a llevar vecinito?
+
+1. Huevos   2. Pollo    3. Carne     4. Harina""")
+
+order= input("Escoja una de las opciones: ")
+order= str(order)
+
+
+
